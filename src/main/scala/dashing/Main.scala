c473d269c87086c39db361c8c1ea081614ab299f
Ben Fradet <benjamin.fradet@gmail.com>
2017-08-29 23:28:52 +0100

    navigation

--- src/main/scala/dashing/Main.scala
+++ src/main/scala/dashing/Main.scala
@@ -2,6 +2,7 @@ package dashing
 
 import org.scalajs.dom
 import japgolly.scalajs.react._, vdom.html_<^._
+import japgolly.scalajs.react.extra._
 import japgolly.scalajs.react.extra.router._
 
 object Main {
@@ -14,7 +15,35 @@ object Main {
       | staticRoute(root, MainDash) ~> render(<.h1("Welcome!"))
     ).notFound(redirectToPage(MainDash)(Redirect.Replace))
       .setTitle(p => s"Dashboard $p | Dashing")
+      .renderWith(layout)
+      .verify(MainDash)
   }
+
+  def layout(c: RouterCtl[Dashboard], r: Resolution[Dashboard]) =
+    <.div(
+      navMenu(c),
+      <.div(^.cls := "container", r.render())
+    )
+
+  val navMenu = ScalaComponent.builder[RouterCtl[Dashboard]]("Menu")
+    .render_P { ctl =>
+      def nav(name: String, target: Dashboard) =
+        <.li(
+          ^.cls := "navbar-brand active",
+          ctl.setOnClick(target),
+          name
+        )
+
+      <.div(
+        ^.cls := "navbar navbar-default",
+        <.ul(
+          ^.cls := "navbar-header",
+          nav("Home", MainDash)
+        )
+      )
+    }
+    .configure(Reusability.shouldComponentUpdate)
+    .build
 
   def main(args: Array[String]): Unit = {
     val router = Router(BaseUrl.fromWindowOrigin_/, routerConfig.logToConsole)
