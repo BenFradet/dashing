730b5cfb77954e6c6e06c2658594797bd1a458ee
Ben Fradet <benjamin.fradet@gmail.com>
2017-09-01 00:04:19 +0100

    chart component

---
+++ src/main/scala/dashing/components/Chart.scala
@@ -0,0 +1,93 @@
+package dashing.components
+
+import japgolly.scalajs.react.vdom.html_<^._
+import japgolly.scalajs.react.{Callback, ScalaComponent}
+
+import org.scalajs.dom.raw.HTMLCanvasElement
+import scala.scalajs.js
+import scala.scalajs.js.JSConverters._
+import scala.scalajs.js.annotation.JSGlobal
+
+@js.native
+trait ChartDataset extends js.Object {
+  def label: String = js.native
+  def data: js.Array[Double] = js.native
+  def fillColor: String = js.native
+  def strokeColor: String = js.native
+}
+
+object ChartDataset {
+  def apply(
+    data: Seq[Double],
+    label: String,
+    backgroundColor: String = "#8080FF",
+    borderColor: String = "#408080"
+  ): ChartDataset =
+    js.Dynamic.literal(
+      label = label,
+      data = data.toJSArray,
+      backgroundColor = backgroundColor,
+      borderColor = borderColor
+    ).asInstanceOf[ChartDataset]
+}
+
+@js.native
+trait ChartData extends js.Object {
+  def labels: js.Array[String] = js.native
+  def datasets: js.Array[ChartDataset] = js.native
+}
+
+object ChartData {
+  def apply(labels: Seq[String], datasets: Seq[ChartDataset]): ChartData =
+    js.Dynamic.literal(
+      labels = labels.toJSArray,
+      datasets = datasets.toJSArray
+    ).asInstanceOf[ChartData]
+}
+
+@js.native
+trait ChartOptions extends js.Object {
+  def responsive: Boolean = js.native
+}
+
+object ChartOptions {
+  def apply(responsive: Boolean): ChartOptions =
+    js.Dynamic.literal(responsive = responsive).asInstanceOf[ChartOptions]
+}
+
+@js.native
+trait ChartConfiguration extends js.Object {
+  def `type`: String = js.native
+  def data: ChartData = js.native
+  def options: ChartOptions = js.native
+}
+
+object ChartConfiguration {
+  def apply(`type`: String, data: ChartData, options: ChartOptions): ChartConfiguration =
+    js.Dynamic.literal(`type` = `type`, data = data, options = options)
+      .asInstanceOf[ChartConfiguration]
+}
+
+@js.native
+@JSGlobal("Chart")
+class JSChart(ctx: js.Dynamic, config: ChartConfiguration) extends js.Object
+
+object Chart {
+  sealed trait ChartStyle
+  case object LineChart extends ChartStyle
+  case class ChartProps(name: String, style: ChartStyle, data: ChartData, width: Int, height: Int)
+
+  val Chart = ScalaComponent.builder[ChartProps]("Chart")
+    .render_P { p =>
+      <.canvas(VdomAttr("width") := p.width, VdomAttr("height") := p.height)
+    }
+    .componentDidMount(scope => Callback {
+      val ctx = scope.getDOMNode.asInstanceOf[HTMLCanvasElement].getContext("2d")
+      scope.props.style match {
+        case LineChart => new JSChart(ctx, ChartConfiguration("line", scope.props.data))
+        case _         => throw new IllegalArgumentException
+      }
+    }).build
+
+  def apply(props: ChartProps) = Chart(props)
+}
