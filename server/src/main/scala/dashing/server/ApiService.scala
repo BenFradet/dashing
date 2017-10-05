4d33ec38e335aee7bccd14ef802d74e338f14ac6
Ben Fradet <benjamin.fradet@gmail.com>
2017-10-05 22:32:42 +0100

    Presenting only the topN

--- server/src/main/scala/dashing/server/ApiService.scala
+++ server/src/main/scala/dashing/server/ApiService.scala
@@ -1,6 +1,7 @@
 package dashing
 package server
 
+import cats.Monoid
 import cats.data.EitherT
 import cats.effect.IO
 import cats.implicits._
@@ -15,23 +16,25 @@ import org.http4s._
 import org.http4s.dsl._
 import scalaj.http.HttpResponse
 
-import shared._
-
 object ApiService extends Service {
 
   val gh = Github(sys.env.get("GITHUB4S_ACCESS_TOKEN"))
   val org = "snowplow"
+  val topN = 5
   val pagOpt = Some(Pagination(1, 1000))
 
   override val service = HttpService[IO] {
     case GET -> Root / "stars" =>
-      getAllStars(org).flatMap(_.fold(ex => NotFound(ex.getMessage), l => Ok(l.asJson.noSpaces)))
+      getTopN(org, topN).flatMap(_.fold(ex => NotFound(ex.getMessage), l => Ok(l.asJson.noSpaces)))
   }
 
-  def getAllStars(org: String): IO[Either[GHException, List[Repo]]] = (for {
+  def getTopN(org: String, n: Int): IO[Either[GHException, List[Repo]]] = (for {
     repos <- EitherT(getRepos(org))
     stars <- EitherT(getStars(org, repos))
-  } yield stars).value
+    sorted = stars.sortBy(-_.stars)
+    topN   = sorted.take(n)
+    rest   = Monoid.combineAll(sorted.drop(n)).copy(repoName = "others")
+  } yield rest :: topN).value
 
   def getRepos(org: String): IO[Either[GHException, List[String]]] = (for {
     repos <- EitherT(
@@ -47,18 +50,6 @@ object ApiService extends Service {
     (for {
       stargazers <- EitherT(
         gh.activities.listStargazers(org, repoName, true, pagOpt).exec[IO, HttpResponse[String]]())
-      stars = stargazers.result
-        .map(_.starred_at)
-        .flatten
-        // we keep only YYYY-MM
-        .map(_.take(7))
-        .sorted
-        .foldLeft((Map.empty[String, Int], 0)) {
-          case ((m, c), month) =>
-            val cnt = m.getOrElse(month, c) + 1
-            (m + (month -> cnt), cnt)
-        }
-    } yield Repo(repoName, stars._1, stars._2)).value
-
-  def buildRepo(repoName: String): Repo = ???
+      repo = Repo(repoName, stargazers.result)
+    } yield repo).value
 }
--- shared/src/main/scala/dashing/shared/package.scala
+++ shared/src/main/scala/dashing/shared/package.scala
@@ -1,5 +1,3 @@
 package dashing
 
-package object shared {
-  final case class Repo(repoName: String, stars: Map[String, Int], maxStars: Int)
-}
+package object shared { }
