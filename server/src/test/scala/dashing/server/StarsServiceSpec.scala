73f4b3d806f821f4eb4b3841934a666121e80190
Ben Fradet <benjamin.fradet@gmail.com>
2017-10-14 12:08:08 +0100

    http4s 0.18.0-M3 for the tests

--- server/src/test/scala/dashing/server/StarsServiceSpec.scala
+++ server/src/test/scala/dashing/server/StarsServiceSpec.scala
@@ -2,7 +2,7 @@ package dashing.server
 
 import cats.effect.IO
 import org.http4s._
-import org.http4s.dsl._
+import org.http4s.dsl.io._
 import org.http4s.testing.IOMatchers
 import org.specs2.mutable.Specification
 
