e1dde3a56bcb9a08affa5cb1bd91136fb0af6be1
Ben Fradet <benjamin.fradet@gmail.com>
2017-11-12 17:07:25 +0000

    Refactored tests to the new config

--- server/src/test/scala/dashing/server/StarsServiceSpec.scala
+++ server/src/test/scala/dashing/server/StarsServiceSpec.scala
@@ -9,7 +9,9 @@ import org.specs2.mutable.Specification
 class StarsServiceSpec extends Specification with IOMatchers {
   args(skipAll = sys.env.get("GITHUB4S_ACCESS_TOKEN").isEmpty)
 
-  def serve(req: Request[IO]): Response[IO] = StarsService.service.orNotFound(req).unsafeRunSync
+  def serve(req: Request[IO]): Response[IO] =
+    StarsService.service(sys.env.getOrElse("GITHUB4S_ACCESS_TOKEN", ""), "igwp", "igwp", 2)
+      .orNotFound(req).unsafeRunSync
 
   "StarsService" should {
     "respond to /stars/top-n" in {
