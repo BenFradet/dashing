fe35eb7ba4573de16bd08d4422ed8f5f7654c858
Ben Fradet <benjamin.fradet@gmail.com>
2018-07-12 19:21:13 +0100

    Make use of EitherT for the pr service

--- server/src/main/scala/dashing/server/PullRequestsService.scala
+++ server/src/main/scala/dashing/server/PullRequestsService.scala
@@ -28,7 +28,7 @@ class PullRequestsService[F[_]: Effect: Timer] extends Http4sDsl[F] {
   )(implicit ec: ExecutionContext): HttpService[F] =
     HttpService[F] {
       case GET -> Root / "prs" => for {
-        prs <- cache.lookupOrInsert("prs", getPRs(Github(Some(token)), org).map(_.map(_.asJson.noSpaces)))
+        prs <- cache.lookupOrInsert("prs", getPRs(Github(Some(token)), org).value.map(_.map(_.asJson.noSpaces)))
         res <- prs.fold(ex => NotFound(ex.getMessage), t => Ok(t))
       } yield res
     }
@@ -36,38 +36,37 @@ class PullRequestsService[F[_]: Effect: Timer] extends Http4sDsl[F] {
 
 object PullRequestsService {
 
-  def getPRs[F[_]: Sync](gh: Github, org: String): F[Either[GHException, GHObjectTimeline]] = (for {
+  def getPRs[F[_]: Sync](gh: Github, org: String): EitherT[F, GHException, GHObjectTimeline] = for {
     repos <- EitherT(utils.getRepos[F](gh, org))
     repoNames = repos.map(_.name)
-    prs <- EitherT(getPRs(gh, org, repoNames))
+    prs <- getPRs(gh, org, repoNames)
     members <- EitherT(utils.getOrgMembers[F](gh, org))
     (prsByMember, prsByNonMember) = prs.partition(pr => members.toSet.contains(pr.author))
     memberPRsCounted = utils.computeTimeline(prsByMember.map(_.created.take(7)))._1
     nonMemberPRsCounted = utils.computeTimeline(prsByNonMember.map(_.created.take(7)))._1
-  } yield GHObjectTimeline(memberPRsCounted, nonMemberPRsCounted)).value
+  } yield GHObjectTimeline(memberPRsCounted, nonMemberPRsCounted)
 
   def getPRs[F[_]: Sync](
     gh: Github,
     org: String,
     repoNames: List[String]
-  ): F[Either[GHException, List[GHObject]]] = (for {
-    nested <- EitherT(
+  ): EitherT[F, GHException, List[GHObject]] = for {
+    nested <-
       repoNames
         .traverse(getPRs(gh, org, _))
         .map(_.sequence)
-    )
     flattened = nested.flatten
-  } yield flattened).value
+  } yield flattened
 
   def getPRs[F[_]: Sync](
-      gh: Github, org: String, repoName: String): F[Either[GHException, List[GHObject]]] =
-    (for {
+      gh: Github, org: String, repoName: String): EitherT[F, GHException, List[GHObject]] =
+    for {
       prs <- EitherT(utils.autoPaginate(p => listPRs(gh, org, repoName, Some(p))))
       pullRequests = prs
         .map(pr => (pr.user.map(_.login), pr.created_at.some).bisequence)
         .flatten
         .map(pr => GHObject(pr._1, pr._2))
-    } yield pullRequests).value
+    } yield pullRequests
 
   def listPRs[F[_]: Sync](
     gh: Github,
