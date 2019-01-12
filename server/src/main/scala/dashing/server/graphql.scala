942fdde37dea8194eeaf9546e987cd122e2a8b64
Ben Fradet <benjamin.fradet@gmail.com>
2019-01-12 21:31:35 +0100

    Make the function automating pagination handle the first page

--- server/src/main/scala/dashing/server/graphql.scala
+++ server/src/main/scala/dashing/server/graphql.scala
@@ -1,14 +1,23 @@
 package dashing.server
 
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+import cats.syntax.option._
 import cats.effect.Sync
-import fs2.{Chunk, Pull, Stream}
+import fs2.{Chunk, Pull}
 
 object graphql {
   def getPRs(owner: String, name: String): String =
     s"""query {repository(owner:\"$owner\",name:\"$name\"){pullRequests(states:[OPEN,CLOSED,MERGED]first:100){edges{node{author{login}createdAt}}pageInfo{endCursor hasNextPage}}}}"""
 
-  def autoPaginate[F[_]: Sync, T <: PageInfo](call: Pagination => F[T]): F[List[T]] =
-    autoPaginate(Pagination(100, None))(call).stream.compile.toList
+  def autoPaginate[F[_]: Sync, T <: PageInfo](call: Pagination => F[T]): F[List[T]] = for {
+    first <- call(Pagination(100, None))
+    rest <-
+      if (first.hasNextPage)
+        autoPaginate(Pagination(100, first.endCursor.some))(call).stream.compile.toList
+      else
+        Sync[F].pure(Nil)
+  } yield first :: rest
 
   private def autoPaginate[F[_], T <: PageInfo](
     pagination: Pagination
@@ -18,8 +27,10 @@ object graphql {
       case _ =>
         Pull.eval(call(pagination)).flatMap { pageInfoLike =>
           Pull.output(Chunk(pageInfoLike)) >> (
-            if (pageInfoLike.hasNextPage) autoPaginate(pagination.copy(cursor = Some(pageInfoLike.endCursor)))(call)
-            else Pull.done
+            if (pageInfoLike.hasNextPage)
+              autoPaginate(pagination.copy(cursor = Some(pageInfoLike.endCursor)))(call)
+            else
+              Pull.done
           )
         }
     }
