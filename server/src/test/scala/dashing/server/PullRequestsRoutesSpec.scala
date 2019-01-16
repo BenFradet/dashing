b078f23ad0ab34fa1d377cdb1c4d804e2f71bf95
Ben Fradet <benjamin.fradet@gmail.com>
2019-01-16 16:35:14 +0100

    Make the existing unit tests work with the interface requiring a client

--- server/src/test/scala/dashing/server/PullRequestsRoutesSpec.scala
+++ server/src/test/scala/dashing/server/PullRequestsRoutesSpec.scala
@@ -3,9 +3,10 @@ package dashing.server
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
-import cats.effect.IO
+import cats.effect.{ContextShift, IO}
 import io.chrisdavenport.mules.{Cache, TimeSpec}
 import org.http4s._
+import org.http4s.client.JavaNetClientBuilder
 import org.http4s.dsl.io._
 import org.http4s.syntax.kleisli._
 import org.http4s.testing.IOMatchers
@@ -17,11 +18,13 @@ class PullRequestsRoutesSpec extends Specification with IOMatchers {
   args(skipAll = sys.env.get("GITHUB_ACCESS_TOKEN").isEmpty)
 
   implicit val timer = IO.timer(global)
+  implicit val cs: ContextShift[IO] = IO.contextShift(global)
 
   def serve(req: Request[IO]): Response[IO] = (for {
     cache <- Cache.createCache[IO, String, String](TimeSpec.fromDuration(12.hours))
+    client = JavaNetClientBuilder[IO](global).create
     service <- new PullRequestsRoutes[IO]()
-      .routes(cache, sys.env.getOrElse("GITHUB_ACCESS_TOKEN", ""),
+      .routes(client, cache, sys.env.getOrElse("GITHUB_ACCESS_TOKEN", ""),
         PRDashboardsConfig(List("igwp"), List.empty, 365.days))
       .orNotFound(req)
   } yield service).unsafeRunSync
