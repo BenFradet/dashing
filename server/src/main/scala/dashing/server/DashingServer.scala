720b79548dac83dc624aec6cbc345602c9b90f6c
Ben Fradet <benjamin.fradet@gmail.com>
2019-02-11 21:29:15 +0100

    Integrate the GraphQL pull requests service

--- server/src/main/scala/dashing/server/DashingServer.scala
+++ server/src/main/scala/dashing/server/DashingServer.scala
@@ -15,7 +15,7 @@ import org.http4s.server.Router
 import org.http4s.server.blaze._
 import org.http4s.syntax.kleisli._
 
-import model.DashingConfig
+import model.{DashingConfig, PageInfo}
 
 object DashingServer extends IOApp {
   import scala.concurrent.ExecutionContext.Implicits.global
@@ -34,10 +34,14 @@ object ServerStream {
         for {
           cache <- Stream.eval(
             Cache.createCache[F, String, String](TimeSpec.fromDuration(c.cacheDuration)))
+          piCache <- Stream.eval(
+            Cache.createCache[F, String, PageInfo](TimeSpec.fromDuration(c.cacheDuration)))
           client <- BlazeClientBuilder[F](ec).stream
+          graphQL = new GraphQL(client, c.ghToken)
           apiService =
             new StarsRoutes[F].routes(cache, c.ghToken, c.starDashboards) <+>
-            new PullRequestsRoutes[F]().routes(cache, c.ghToken, c.prDashboards)
+            new PullRequestsRoutes[F]().routes(cache, c.ghToken, c.prDashboards) <+>
+            new PullRequestsRoutes2[F]().routes(piCache, graphQL, c.prDashboards)
           httpApp = Router(
             "/" -> new RenderingRoutes[F](ec).routes,
             "/api" -> apiService
